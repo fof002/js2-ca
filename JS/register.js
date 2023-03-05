603f6875ec45daafa80722445ed3633c5b96e684
fof002 <80104002+fof002@users.noreply.github.com>
2023-03-05 08:02:18 +0100

    finished reigster user
    Added field validation

--- JS/register.js
+++ JS/register.js
@@ -6,16 +6,19 @@ const nameInput = document.querySelector("#name");
 const emailInput = document.querySelector("#email");
 const passwordInput = document.querySelector("#password");
 const errorContainer = document.querySelector("#error-container");
+const errorUl = document.querySelector("#error-ul");
 let userInput = {};
-
 // -----------------registrers user eventlistener
 registerButton.addEventListener("click", () => {
-  if (!nameInput.value || !emailInput.value || !passwordInput.value) {
-    errorContainer.innerHTML = `Fill out all the fields`;
-  } else {
-    errorContainer.innerHTML = "";
+  if (
+    nameInput.value.trim().length < 20 &&
+    emailInput.value.includes("@noroff.no") &&
+    passwordInput.value.trim().length >= 8
+  ) {
     createUserInput();
     registerUser();
+  } else {
+    errorContainer.innerHTML = `Check user registration demands below`;
   }
 });
 //----------------------------------
@@ -39,8 +42,6 @@ async function registerUser() {
     } else {
       const errorArray = json.errors;
       errorArray.forEach((errormessage) => {
-        const errorUl = document.createElement("ul");
-        errorContainer.appendChild(errorUl);
         errorUl.innerHTML += `<li>${errormessage.message}</li>`;
       });
     }
@@ -58,3 +59,28 @@ const createUserInput = () => {
   };
 };
 //---------------------------------------
+
+//validate input in registration fields
+document.addEventListener("keydown", validateUserInput);
+
+function validateUserInput() {
+  errorUl.innerHTML = "";
+  errorContainer.innerHTML = "";
+  if (passwordInput.value.trim().length < 8) {
+    errorUl.innerHTML += `<li>Password must be at least 8 characters</li>`;
+  }
+  if (!emailInput.value.includes("@noroff.no")) {
+    errorUl.innerHTML += `<li>Only noroff.no emails are allowed to register</li>`;
+  }
+  if (nameInput.value.trim().length > 20) {
+    errorUl.innerHTML += `<li>Name cannot be greater than 20 characters</li>`;
+  }
+  if (
+    nameInput.value.trim().length < 20 &&
+    emailInput.value.includes("@noroff.no") &&
+    passwordInput.value.trim().length >= 8
+  ) {
+    errorContainer.innerHTML = `Looks good!`;
+  }
+}
+//-----------------------------------------------
